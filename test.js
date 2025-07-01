21d756d8cd3297d579bf3b372a87e35965931c76
Darren <darrenthebozz1@gmail.com>
2025-07-01 09:17:06 +0100

    Remove junk file

--- test.js
+++
@@ -1,7 +0,0 @@
-const fs  = require("fs/promises")
-
-async function start() {
-    const eventAutoScalingCamps = JSON.parse((await fs.readFile("./items/buildings.json")).toString())
-    console.log(eventAutoScalingCamps)
-}
-start()
